32c917e46fe65965ffd2b9a19c1b43f17175ce4d
Cosmin Romanescu <cosmindaniel134@yahoo.com>
2023-06-21 21:12:46 +0300

    first commit

---
+++ src/main/java/com/sda/onlineBets/OnlineBetsApplication.java
@@ -0,0 +1,13 @@
+package com.sda.onlineBets;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnlineBetsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OnlineBetsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sda/onlineBets/OnlineBetsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sda.onlineBets;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OnlineBetsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
